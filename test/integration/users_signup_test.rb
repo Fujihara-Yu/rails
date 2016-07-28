3c7236b9c800ca1c5060efccd0e4687265039070
Ｙｕ　Ｆｕｊｉｈａｒａ <mamenosiba2010@yahoo.co.jp>
2016-07-28 18:06:51 +0000

    Finish 8syou

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -2,6 +2,7 @@ require 'test_helper'
 
 class UsersSignupTest < ActionDispatch::IntegrationTest
 
+  
   test "invalid signup information" do
     get signup_path
     assert_no_difference 'User.count' do
@@ -21,5 +22,6 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                             password_confirmation: "password" }
     end
     assert_template 'users/show'
+    assert is_logged_in?
   end
 end
