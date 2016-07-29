ab5590a42cc57e601e58cd318fb9c03fc733ea7b
Ｙｕ　Ｆｕｊｉｈａｒａ <mamenosiba2010@yahoo.co.jp>
2016-07-29 06:27:43 +0000

    Finish 9syou

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,8 +6,9 @@ class User < ActiveRecord::Base
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
-  has_secure_password
-  validates :password, presence: true, length: { minimum: 6 }
+   has_secure_password
+  validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
+
 
   # 与えられた文字列のハッシュ値を返す
   def User.digest(string)
